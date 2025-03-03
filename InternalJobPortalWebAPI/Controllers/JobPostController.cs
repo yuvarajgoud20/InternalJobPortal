7df8cdfa84b04a88f77bbf85bde2b8ba9adac5de
Sharvani Narayana <sharvani.narayana@zelis.com>
2025-03-03 12:12:32 +0530

    added EFJobPostRepo,IJobPostRepo,JobPostController

---
+++ InternalJobPortalWebAPI/Controllers/JobPostController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using JobPostLibrary.Models;
+using JobPostLibrary.Repos;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using JobPostLibrary;
+
+namespace JobPostWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+  
+    public class JobPostController : ControllerBase
+    {
+        private readonly IJobPostRepoAsync jobRepo;
+
+        public JobPostController(IJobPostRepoAsync repository)
+        {
+            jobRepo = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            List<JobPost> jobPosts = await jobRepo.GetAllJobPostsAsync();
+            return Ok(jobPosts);
+        }
+
+        [HttpGet("{postId}")]
+        public async Task<ActionResult> GetOne(int postId)
+        {
+            try
+            {
+                JobPost jobPost = await jobRepo.GetJobPostAsync(postId);
+                return Ok(jobPost);
+            }
+            catch (InternalJobPortalException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet("ByJobId/{jobId}")]
+        public async Task<ActionResult> GetByJobId(string jobId)
+        {
+            try
+            {
+                List<JobPost> jobPosts = await jobRepo.GetJobPostsByJobIdAsync(jobId);
+                return Ok(jobPosts);
+            }
+            catch (InternalJobPortalException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Insert(JobPost jobPost)
+        {
+            try
+            {
+                await jobRepo.AddJobPostAsync(jobPost);
+                return Created($"api/JobPost/{jobPost.PostID}", jobPost);
+            }
+            catch (InternalJobPortalException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{postId}")]
+        public async Task<ActionResult> Update(int postId, JobPost jobPost)
+        {
+            try
+            {
+                await jobRepo.UpdateJobPostAsync(postId, jobPost);
+                return Ok(jobPost);
+            }
+            catch (InternalJobPortalException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{postId}")]
+        public async Task<ActionResult> Delete(int postId)
+        {
+            try
+            {
+                await jobRepo.DeleteJobPostAsync(postId);
+                return Ok();
+            }
+            catch (InternalJobPortalException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
