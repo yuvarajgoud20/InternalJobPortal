c35bc7ffb7980d4458a0d876be13c5d675e32662
Manipreethi Gangisetty <manipreethi.gangisetty@zelis.com>
2025-03-03 12:24:07 +0530

    Added JobSkill Repo and JobSkillController

---
+++ InternalJobPortalLibrary/Repos/IJobSkillRepoAsync.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InternalJobPortalLibrary.Models;
+
+namespace InternalJobPortalLibrary.Repos
+{
+    public interface IJobSkillRepoAsync
+    {
+        Task<List<JobSkill>> GetAllJobSkillsAsync();
+        Task<JobSkill> GetJobSkillAsync(string JobID, string SkillID);
+        Task<List<JobSkill>> GetJobSkillsByJobIDAsync(string jobID);
+        Task<List<JobSkill>> GetJobSkillsBySkillIDAsync(string skillID);
+        Task AddJobSkillAsync(JobSkill jobSkill);
+        Task UpdateJobSkillAsync(string JobID, string SkillID, JobSkill jobSkill);
+        Task DeleteJobSkillAsync(string JobID, string SkillID);
+    }
+}
