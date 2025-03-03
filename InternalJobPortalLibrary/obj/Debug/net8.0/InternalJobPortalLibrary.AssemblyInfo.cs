6428ae68f978c00362bb4e029546d7c5c45b4f4a
Arif Shaik Mahammed <arif.shaikmahammed@zelis.com>
2025-03-03 11:40:05 +0530

    Added Repo Folder

--- InternalJobPortalLibrary/obj/Debug/net8.0/InternalJobPortalLibrary.AssemblyInfo.cs
+++ InternalJobPortalLibrary/obj/Debug/net8.0/InternalJobPortalLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InternalJobPortalLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c75a89b8f1031d789368a6fd9a5a9941e80219fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1feed3ad24cfd4bd4973ef3aa5734e53ff91635c")]
 [assembly: System.Reflection.AssemblyProductAttribute("InternalJobPortalLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InternalJobPortalLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
