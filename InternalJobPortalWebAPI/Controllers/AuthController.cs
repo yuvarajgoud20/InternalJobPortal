aa30fa335c65b6ea43ebdf41b85167f5686f1330
Arif Shaik Mahammed <arif.shaikmahammed@zelis.com>
2025-03-04 11:31:49 +0530

    Auth Controller

---
+++ InternalJobPortalWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AirlinesWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private string GenerateJWT(string userName, string role, string secretKey)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            Claim[] claims = new[] {
+                new Claim(ClaimTypes.Name, userName),
+                new Claim(ClaimTypes.Role, role)
+            };
+            var token = new JwtSecurityToken(
+                issuer: "https://www.manipreethi.com",
+                audience: "https://www.manipreethi.com",
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: credentials,
+                claims: claims
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        [HttpGet("{userName}/{role}/{secretKey}")]
+        public string GetToken(string userName, string role, string secretKey)
+        {
+            string token = GenerateJWT(userName, role, secretKey);
+            return token;
+        }
+
+    }
+}
--- InternalJobPortalWebAPI/Program.cs
+++ InternalJobPortalWebAPI/Program.cs
@@ -1,6 +1,9 @@
 
+using System.Text;
 using InternalJobPortalLibrary.Repos;
 using JobPostLibrary.Repos;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace InternalJobPortalWebAPI
 {
@@ -23,6 +26,24 @@ namespace InternalJobPortalWebAPI
             builder.Services.AddScoped<IJobPostRepoAsync, EFJobPostRepoAsync>();
             builder.Services.AddScoped<IEmployeeRepoAsync, EFEmployeeRepoAsync>();
             builder.Services.AddScoped<IEmployeeSkillRepoAsync, EFEmployeeSkillRepoAsync>();
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = "https://www.snrao.com",
+                    ValidAudience = "https://www.snrao.com",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Johny Johny yes papa....open your laptop HAHAHA!!!"))
+                };
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -31,7 +52,7 @@ namespace InternalJobPortalWebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
